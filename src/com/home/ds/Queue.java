fc9a16b8a0ac8d8b295d6e30d6a8839ceda38a7e
masterfile <masterfile@masterfile-PC>
2016-07-09 22:03:32 +0530

    Queue initial implementation

---
+++ src/com/home/ds/Queue.java
@@ -0,0 +1,40 @@
+package com.home.ds;
+
+public class Queue<E> {
+
+	private static class Node<E> {
+		private E ele;
+		private Node<E> next;
+		public Node(E ele) {
+			this.ele = ele;
+		}
+	}
+	
+	private Node<E> head;
+	private Node<E> tail;
+	private int count;
+	
+	public void enq(E ele) {
+		Node<E> newNode = new Node<>(ele);
+		if ((head = tail) == null) {
+			head = tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+		count++;
+	}
+	
+	public E deq() {
+		if (head == null) {
+			throw new RuntimeException("Empty queue");
+		} else {
+			E ele = head.ele;
+			head = head.next;
+			if (--count == 0) {
+				tail = null;
+			}
+			return ele;
+		}
+	}
+}
