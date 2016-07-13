04b20851c29df80152254a9f3f35fc0775d137c9
masterfile <masterfile@masterfile-PC>
2016-07-14 00:22:53 +0530

    junit suite

---
+++ test/com/home/ds/AllTests.java
@@ -0,0 +1,11 @@
+package com.home.ds;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ LinkedListTest.class, QueueTest.class, StackTest.class })
+public class AllTests {
+
+}
